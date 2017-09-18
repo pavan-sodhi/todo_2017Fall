a41b3a7e2a8b025dfce7de26591aa00b57a38ee8
Pavandeep Singh <ps355@afsaccess2.njit.edu>
2017-09-18 18:23:16 -0400

    third commit

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
-         echo "<h1>Hello, PHP!</h1>";
-	       ?>
+         echo "<h1> Pavandeep Singh's Home work
+<br>	 </h1>";
+		echo <h2> New Jersey <br> </h2>
+		echo 	<h3><br> no content</h3>
+
+
+?>
