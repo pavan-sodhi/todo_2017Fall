879cadde878b518112378dd3ea115e43202c57f8
Pavandeep Singh <ps355@afsaccess2.njit.edu>
2017-09-18 19:23:22 -0400

    added index.php and css

--- index.php
+++ index.php
@@ -1,8 +1,26 @@
-<?php
-         echo "<h1> Pavandeep Singh's Home work
-<br>	 </h1>";
-		echo <h2> New Jersey <br> </h2>
-		echo 	<h3><br> no content</h3>
+<!doctype html>
 
+<html lang="en">
+ <head>
+    <meta charset="utf-8">
 
-?>
+   	 <title>Pavandeep Singh IS 601 Homework </title>
+     		 <meta name="description" content="Assignment One">
+        	<meta name="author" content="SitePoint">
+
+	  	<link rel="stylesheet" href="css/styles.css?v=1.0">
+
+	    <!--[if lt IE 9]>
+	        <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+		  <![endif]-->
+  </head>
+
+		  <body>
+		    <script src="js/scripts.js"></script>
+		    <h1>Pavandeep Singh' IS 601 Website </h1> <br>
+		    <h2><br> First Assignment <br> </h2>
+		   <br> <a href="http://www.njit.edu/">New Jersey Institute of
+		    Technology </a>
+		    <br>
+		    </body>   
+</html>
